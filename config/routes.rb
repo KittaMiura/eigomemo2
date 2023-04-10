4a5abd99d739141852837bb24cbb225520fe0f9a
KittaMiura <112059987+KittaMiura@users.noreply.github.com>
2023-04-10 16:04:27 +0900

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admins, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
-    }
 
   root to: 'homes#top'
   get "about" => "homes#about"
+
+  #ログイン機能
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
 end
