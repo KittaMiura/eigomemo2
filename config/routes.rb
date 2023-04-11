1a1bb402258316ada056f918b9eea7411a65af21
KittaMiura <112059987+KittaMiura@users.noreply.github.com>
2023-04-11 11:03:03 +0900

    一旦ユーザー削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: 'homes#top'
+  get "top" => 'homes#top'
   get "about" => "homes#about"
-
-  #ログイン機能
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
 
 end
